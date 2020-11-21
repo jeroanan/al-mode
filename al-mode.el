fd4dc056b643b31519c0f40525e456ea2eaed84e
David Wilson <davidwil@posteo.de>
2020-11-21 15:07:52 +0000

    Add documentation to all functions

--- al-mode.el
+++ al-mode.el
@@ -280,30 +280,32 @@
 	      `((,x-control-keywords-regexp . font-lock-keyword-face))))
 
 (defvar al-assignments)
+;; The regexp contained here are all the ways of assigning a value to a variable
 (setq-local al-assignments
 	    '(("[:|+|-|/|*]=" . font-lock-keyword-face)))
 
 (defvar al-string-text)
+;; Anything inside single quotes is a string
 (setq-local al-string-text
 	    '(("'.*'" . font-lock-string-face)))
 
 (defvar al-single-line-comment)
+;; If a line contains // then it denotes the start of a single-line comment
 (setq-local al-single-line-comment
 	    '((".*\\(//.*\\)" . (1 font-lock-comment-face))))
 
+(defvar al-highlights)
 ;; The different types of syntax highlight we want to apply in order
 ;; of priority. SO for instance we want to apply al-single-line-comment before
 ;; all other comment types.
-(defvar al-highlights)
 (setq al-highlights (append al-single-line-comment
 			    al-string-text
 			    al-assignments
 			    al-font-lock-keywords))
 
+(defvar al-mode-syntax-table nil "Syntax table for `al-mode'.")
 ;; Handle pesky multiline comments -- from
 ;; http://ergoemacs.org/emacs/elisp_comment_coloring.html
-(defvar al-mode-syntax-table nil "Syntax table for `al-mode'.")
-
 (setq al-mode-syntax-table
       (let ( (synTable (make-syntax-table)))
         ;; comment style “/* … */”
@@ -314,6 +316,7 @@
         synTable))
 
 (defun al-get-line-content ()
+  "Get the current line as a string."
   (save-excursion
     (beginning-of-line)
     (setq p1 (point))
@@ -322,23 +325,20 @@
   (buffer-substring-no-properties p1 p2))
 
 (defun al-goto-previous-non-blank-line ()
+  "Move the point up to the first non-blank line it encounters."
   (previous-line)
   (setq line-content (al-get-line-content))
   (when (and (string= "" (string-trim line-content))
 	     (/= (line-number-at-pos) 1))
     (al-goto-previous-non-blank-line)))
 
-(defun al-get-previous-non-blank-line ()
-  (save-excursion
-    (al-goto-previous-non-blank-line)
-    (al-get-line-content)))
-
 (defun al-goto-first-character-on-line ()
+  "Move the point to the first non-whitespace character on the current line."
   (beginning-of-line)
   (skip-chars-forward " \t\r"))
 
 (defun al-delete-leading-whitespace ()
-  "Delete leading whitespace from the current line"
+  "Delete leading whitespace from the current line."
   (save-excursion
     (beginning-of-line)
     (setq-local p1 (point))
@@ -348,35 +348,37 @@
       (delete-region p1 p2))))
 
 (defun al-get-current-line-indent ()
+  "Get the column number of the current line's indent."
   (save-excursion
     (al-goto-first-character-on-line)
     (current-column)))
 
 (defun al-get-previous-line-indent ()
+  "Get the column number of the previous line's indent."
   (save-excursion
     (al-goto-previous-non-blank-line)
     (al-get-current-line-indent)))
 
 (defun al-get-current-line-last-token ()
+  "Get the last word on the current line."
   (string-trim (car (last
 		     (split-string (al-get-line-content) " ")))))
 
 (defun al-get-previous-line-last-token ()
+  "Get the last word of the previous line."
   (save-excursion
     (al-goto-previous-non-blank-line)
     (al-get-current-line-last-token)))
 
 (defun al-goto-line-where-last-token-is (token)
+  "Move point down to the next line that ends with TOKEN."
   (forward-line -1)
   (unless (string= token (al-get-current-line-last-token))
     (al-goto-line-where-last-token-is token)))
 
-(defun al-get-indent-on-last-line-ending-with-token (token)
-  (save-excursion
-    (al-goto-line-where-last-token-is token)
-    (al-get-current-line-indent)))
-
 (defun al-goto-start-of-block (block-alist-entry current-level)
+  "Move point to the end of block according to BLOCK-ALIST-ENTRY.
+CURRENT-LEVEL should be zero when called."
   (let ((block-begin (car block-alist-entry))
 	(block-end (cdr block-alist-entry)))
     (forward-line -1)
@@ -390,30 +392,37 @@
        (t (al-goto-start-of-block block-alist-entry current-level))))))
 
 (defun al-get-indent-at-start-of-block (block-alist-entry)
+  "Get the indent level at the start of the current block according to BLOCK-ALIST-ENTRY."
   (save-excursion
     (al-goto-start-of-block block-alist-entry 0)
     (al-get-current-line-indent)))
 
 (defvar begin-end-blocks)
+"Alist of block beginning and endings.
+Entries ending in nil are 'beginning-only'."
 (setq begin-end-blocks '(("{" . "}")
 			 ("begin" . "end;")
 			 ("var" . nil)))
 
 (defun al-last-line-begins-block? ()
+  "Is the last token of the previous line the begining of a block?"
   (let ((previous-token (al-get-previous-line-last-token)))
     (member previous-token (map-keys begin-end-blocks))))
 
 (defun al-this-line-ends-block? ()
+  "Does this line close a block?"
   (let ((last-token (al-get-current-line-last-token))
 	(matching-tokens (map-values begin-end-blocks)))
     (member last-token matching-tokens)))
 
 (defun al-indent-for-new-block ()
+  "Indent the current line when it is the start of a block."
   (save-excursion
     (al-goto-first-character-on-line)
     (indent-to (indent-next-tab-stop (al-get-previous-line-indent)))))
 
 (defun al-indent-end-block ()
+  "Indent the current line when it is the end of a block."
   (save-excursion
     (let* ((this-token (al-get-current-line-last-token))
 	   (block-alist-entry (rassoc this-token begin-end-blocks))
@@ -422,13 +431,14 @@
       (indent-to starting-indent))))
 
 (defun al-indent-same-as-previous-line ()
-  "Indent this line to the same level as the last non-empty one"
+  "Indent this line to the same level as the last non-empty one."
   (save-excursion
     (let ((indent-level (al-get-previous-line-indent)))
       (al-goto-first-character-on-line)
       (indent-to indent-level))))
 
 (defun al-indent-line ()
+  "Indent the current line."
   (al-delete-leading-whitespace)
   (cond
    ((= (line-number-at-pos) 1) nil)
